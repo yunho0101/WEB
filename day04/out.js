a10398e00f409441b7d46b360f2a06b13f19eed9
정윤호 <52642517+yunho0101@users.noreply.github.com>
2023-02-02 17:37:03 +0900

    Add files via upload

---
+++ day04/out.js
@@ -0,0 +1,20 @@
+function 빨래하다() {
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물에 집어 넣는다.")
+    console.log("3. 비누를 바른다.")
+    console.log("4. 오물조물 비빈다.")
+    console.log("5. 물에 여러번 헹군다.")
+    console.log("6. 짠다.")
+    console.log("7. 넌다.")
+ }
+
+ //청소하다로 해보기
+ function 청소하다() {
+    console.log("1. 바닥을 쓴다")
+    console.log("2. 청소기를 돌린다")
+    console.log("3. 물걸레를 빤다")
+    console.log("4. 물걸레로 닦는다.")
+    console.log("5. 물걸레를 빤다.")
+    console.log("6. 물걸레를 짠다.")
+    console.log("7. 물걸레를 넌다.")
+ }
---
+++ day04/out2.js
@@ -0,0 +1,10 @@
+function 여행하다() {
+    console.log("1. 여행지를 고른다")
+    console.log("2. 이동수단을 고른다.")
+    console.log("3. 시간에 맞춰 떠난다.")
+ }
+ function 쉬다() {
+    console.log("1. 넷플릭스 시청")
+    console.log("2. 컴퓨터 게임")
+    console.log("3. 잔다")
+ }
