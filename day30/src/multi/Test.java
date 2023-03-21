ecfa2692d05f190c2839f2f68801a784a5417988
정윤호 <52642517+yunho0101@users.noreply.github.com>
2023-03-21 11:33:51 +0900

    Add files via upload

---
+++ day30/src/multi/Test.java
@@ -0,0 +1,26 @@
+package multi;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Test")
+public class Test extends HttpServlet {
+	
+    
+	public void init(ServletConfig config) throws ServletException {
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
