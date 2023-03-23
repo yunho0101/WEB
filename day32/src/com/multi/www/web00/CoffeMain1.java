79fbfa32abfbde673fe3847df5a83e360be342b4
정윤호 <52642517+yunho0101@users.noreply.github.com>
2023-03-24 00:13:46 +0900

    Add files via upload

---
+++ day32/src/com/multi/www/web00/CoffeMain1.java
@@ -0,0 +1,15 @@
+package com.multi.www.web00;
+
+public class CoffeMain1 {
+
+	public static void main(String[] args) {
+		Car car= new Car("빨강",10,200);
+		Car car2= new Car("파랑",20,250);
+		Car car3= new Car("검정",30,300);
+		System.out.println(car);
+		System.out.println(car2);
+		System.out.println(car3);
+
+	}
+
+}
