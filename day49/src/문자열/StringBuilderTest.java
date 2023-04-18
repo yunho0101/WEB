f5a774b97d8e8413a057b62f43aba98ca774e543
정윤호 <52642517+yunho0101@users.noreply.github.com>
2023-04-18 09:06:22 +0900

    Add files via upload

---
+++ day49/src/문자열/StringBuilderTest.java
@@ -0,0 +1,20 @@
+package 문자열;
+
+public class StringBuilderTest {
+
+	public static void main(String[] args) {
+		String s = "pongpong";
+		String s2 = "pongpong";
+		System.out.println(s == s2); //주소비교
+		System.out.println(s.equals(s2));//주소가 가르키는 값비교
+		
+		s = "hahaha";
+		System.out.println(s == s2); //주소비교
+		System.out.println(s.equals(s2));//주소가 가르키는 값비교
+		
+		String s3 = s.substring(2);
+		
+		
+	}
+
+}
---
+++ day49/src/문자열/Test.java
@@ -0,0 +1,11 @@
+package 문자열;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Integer x = 100;
+		int x2 = x;
+		
+	}
+
+}
